6ba12851c8fd73896dd9d09a1be8dd5d3298609f
Mohannadk28 <73769579+Mohannadk28@users.noreply.github.com>
2023-02-28 20:53:39 +0300

    backup

--- config.def.h
+++ config.def.h
@@ -228,7 +228,6 @@ static const Key keys[] = { // Keyborad Bindings
 static const Button buttons[] = { // Mouse Bindings
   { ClkTagBar,            MODKEY,         Button1,        tag,            {0            } },
   { ClkTagBar,            MODKEY,         Button3,        toggletag,      {0            } },
-  { ClkWinTitle,          0,              Button2,        zoom,           {0            } },
   { ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
   { ClkClientWin,         MODKEY,         Button1,        movemouse,      {0            } },
   { ClkClientWin,         MODKEY,         Button2,        togglefloating, {0            } },
