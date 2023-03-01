cf51273e3bc528a2707dab8c030d117a048931e5
Mohannadk28 <73769579+Mohannadk28@users.noreply.github.com>
2023-03-01 15:10:36 +0300

    hmmmm

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -6,7 +6,8 @@ brightnessctl s 100%
 mpv --no-video ~/.pixelfiles/sounds-pixelfiles/startup.mp3 &
 
 /usr/bin/lxpolkit & # LX PolicyKit
-feh --no-fehbg --bg-fill -z ~/.pixelfiles/wallpapers-pixelfiles/* # Sets The Wallpaper
+#feh --no-fehbg --bg-fill -z ~/.pixelfiles/wallpapers-pixelfiles/* # Sets The Wallpaper
+wal -i "/home/mohannadk28/.pixelfiles/wallpapers-pixelfiles/wallhaven-mdr8o1.jpg"
 dwmblocks & # Starts DWM Blocks
 emacs --daemon &
 
